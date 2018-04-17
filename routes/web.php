c40a611fe20ae538ecb5b12b6c19441b2f193c69
Kenold Beauplan <bkenold@gmail.com>
2018-04-17 12:49:45 -0400

    add post create route

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,9 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+
+Route::get('/post/create', [
+    'uses' => 'PostController@create',
+    'as' => 'post.create'
+]);
