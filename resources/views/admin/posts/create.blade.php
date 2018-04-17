3c8028b1f8402bb5e6222b0b03370c314373f017
Kenold Beauplan <bkenold@gmail.com>
2018-04-17 15:01:13 -0400

    add create form

--- resources/views/admin/posts/create.blade.php
+++ resources/views/admin/posts/create.blade.php
@@ -1,6 +1,28 @@
 @extends('layouts.app')
 
 @section('content')
-    <h2>Create new post</h2>
+    <div class="card">
+        <div class="card-header">Create a new post</div>
 
+        <div class="card-body">
+            <form action="{{ route('post.store') }}" method="POST">
+                {{ csrf_field() }}
+                <div class="form-group">
+                    <label for="title">Title</label>
+                    <input type="text" name="title" class="form-control">
+                </div>
+                <div class="form-group">
+                    <label for="featured">Feautured Image</label>
+                    <input type="file" name="featured" class="form-control">
+                </div>
+                <div class="form-group">
+                    <label for="content">Content</label>
+                    <textarea name="content" id="contnet" cols="5" rows="5" class="form-control"></textarea>
+                </div>
+                <div class="form-group">
+                    <button type="submit" class="btn btn-success">Submit</button>
+                </div>
+            </form>
+        </div>
+    </div>
 @endsection
