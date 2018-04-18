621d3876700d69f8dae3db596600f8598e70dc51
Kenold Beauplan <bkenold@gmail.com>
2018-04-18 13:27:59 -0400

    add new link

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -73,7 +73,8 @@
                     <div class="col-lg-4">
                         <ul class="list-group">
                             <li class="list-group-item"><a href="{{ route('home') }}">Home</a></li>
-                             <li class="list-group-item"><a href="{{ route('category.create') }}">Create new category</a></li>
+                            <li class="list-group-item"><a href="{{ route('category.create') }}">Create new category</a></li>
+                            <li class="list-group-item"><a href="{{ route('categories') }}">Categories</a></li>
                             <li class="list-group-item"><a href="{{ route('post.create') }}">Create new post</a></li>
                         </ul>
                     </div>
