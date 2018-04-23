69d844feb67e2ebbd040ed432bd6b574485d3d11
Kenold Beauplan <bkenold@gmail.com>
2018-04-23 17:00:22 -0400

    initial commit

---
+++ resources/views/admin/posts/trashed.blade.php
@@ -0,0 +1,28 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="card">
+        <div class="card-header">Trashed Posts</div>
+
+        <div class="card-body">
+            <table class="table table-hover">
+                <thead>
+                    <th>Title</th>
+                    <th>Image</th>
+                    <th colspan="3">Actions</th>            
+                </thead>
+                <tbody>
+                    @foreach($posts as $post)
+                        <tr>
+                            <td><img src="{{ $post->featured }}" alt="{{ $post->title }}" height="50px"></td>
+                            <td>{{ $post->title }}</td>
+                            <td><a href="">Edit</a></td>
+                            <td><a href="{{ route('post.delete', ['id' => $post->id]) }}">Restore</a></td>
+                            <td><a href="{{ route('post.kill', ['id' => $post->id]) }}">Destroy</a></td>
+                        </tr>                
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
+    </div>    
+@endsection
