16e7c8202e29adba0060b4e3acb98e65665772ba
Kenold Beauplan <bkenold@gmail.com>
2018-04-23 16:29:37 -0400

    display posts

---
+++ resources/views/admin/posts/index.blade.php
@@ -0,0 +1,27 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="card">
+        <div class="card-header">Posts</div>
+
+        <div class="card-body">
+            <table class="table table-hover">
+                <thead>
+                    <th>Title</th>
+                    <th>Image</th>
+                    <th colspan="2">Actions</th>            
+                </thead>
+                <tbody>
+                    @foreach($posts as $post)
+                        <tr>
+                            <td><img src="{{ $post->featured }}" alt="{{ $post->title }}" height="50px"></td>
+                            <td>{{ $post->title }}</td>
+                            <td><a href="">Edit</a></td>
+                            <td><a href="">Delete</a></td>
+                        </tr>                
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
+    </div>    
+@endsection
