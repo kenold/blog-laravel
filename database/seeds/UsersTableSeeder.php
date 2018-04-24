74ea8c66d2360ccbb0b8946ea3e056229f3fd9e0
Kenold Beauplan <bkenold@gmail.com>
2018-04-24 16:48:58 -0400

    create default profile

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -11,10 +11,19 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
-        App\User::create([
+        $user = App\User::create([
             'name' => 'Kenold Beauplan',
             'email' => 'something4ken@gmail.com',
-            'password' => bcrypt('mabekeke')
+            'password' => bcrypt('mabekeke'),
+            'admin' => 1
+        ]);
+
+        App\Profile::create([
+            'user_id' => $user->id,
+            'avatar' => 'uploads/avatars/avatar.jpg',
+            'about' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore placeat libero minima eveniet, magni doloremque nulla suscipit ab. Eius harum voluptatibus quaerat rerum eveniet, sint velit suscipit. Ipsa, quidem et!',
+            'facebook' => 'facebook.com',
+            'youtube' => 'youtube.com'
         ]);
     }
 }
