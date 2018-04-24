e7b7fa2dee86c53c69bc0f1563359cd3c03438fe
Kenold Beauplan <something4ken@gmail.com>
2018-04-24 19:41:41 -0400

    fix model namespace

--- database/factories/PostFactory.php
+++ database/factories/PostFactory.php
@@ -2,7 +2,7 @@
 
 use Faker\Generator as Faker;
 
-$factory->define(Model::class, function (Faker $faker) {
+$factory->define(App\Post::class, function (Faker $faker) {
     return [
         'title' => $faker->sentence(6),
         'slug' => str_slug('title', '-'),
--- database/factories/TagFactory.php
+++ database/factories/TagFactory.php
@@ -2,7 +2,7 @@
 
 use Faker\Generator as Faker;
 
-$factory->define(Tag::class, function (Faker $faker) {
+$factory->define(App\Tag::class, function (Faker $faker) {
     return [
         'tag' => $faker->randomElement(['Konpa', 'Sounds', 'Urban', 'Live'])
     ];
