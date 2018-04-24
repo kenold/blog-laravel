4079211f6d6267e0c5d14696ee26e28b4c3637ef
Kenold Beauplan <bkenold@gmail.com>
2018-04-24 11:40:32 -0400

    initial commit

---
+++ resources/views/admin/posts/edit.blade.php
@@ -0,0 +1,46 @@
+@extends('layouts.app')
+
+@section('content')
+    @if(count($errors) > 0)
+        <ul class="list-group mb-2">
+            @foreach($errors->all() as $error)
+                <li class="list-group-item text-danger">
+                    {{ $error }}
+                </li>
+            @endforeach
+        </ul>
+    @endif
+
+    <div class="card">
+        <div class="card-header">Update Post</div>
+
+        <div class="card-body">
+            <form action="{{ route('post.update', ['id' => $post->id]) }}" method="POST" enctype="multipart/form-data">
+                {{ csrf_field() }}
+                <div class="form-group">
+                    <label for="title">Title</label>
+                    <input type="text" name="title" value="{{ $post->title }}" class="form-control">
+                </div>
+                <div class="form-group">
+                    <label for="featured">Feautured Image</label>
+                    <input type="file" name="featured" class="form-control">
+                </div>
+                <div class="form-group">
+                    <label for="category">Select a Category</label>
+                    <select name="category_id" id="category" class="form-control">
+                        @foreach($categories as $category)
+                            <option value="{{ $category->id }}">{{ $category->name }}</option>
+                        @endforeach
+                    </select>
+                </div>
+                <div class="form-group">
+                    <label for="content">Content</label>
+                    <textarea name="content" id="content" cols="5" rows="5" class="form-control">{{ $post->content }}</textarea>
+                </div>
+                <div class="form-group">
+                    <button type="submit" class="btn btn-success">Submit</button>
+                </div>
+            </form>
+        </div>
+    </div>
+@endsection
