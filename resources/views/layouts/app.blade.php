b0e64061bb0c06916b0a607fc8b2cc68f0e2a3bf
Kenold Beauplan <bkenold@gmail.com>
2018-04-25 12:43:32 -0400

    hide users section for non-admins

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -74,9 +74,11 @@
                     <div class="col-lg-4">
                         <ul class="list-group">
                             <li class="list-group-item"><a href="{{ route('home') }}">Home</a></li>
-                            <li class="list-group-item"><strong>USERS</strong></li>
-                            <li class="list-group-item"><a href="{{ route('users') }}">All Users</a></li>
-                            <li class="list-group-item"><a href="{{ route('user.create') }}">New User</a></li>
+                            @if(Auth::user()->admin)
+                                <li class="list-group-item"><strong>USERS</strong></li>
+                                <li class="list-group-item"><a href="{{ route('users') }}">All Users</a></li>
+                                <li class="list-group-item"><a href="{{ route('user.create') }}">New User</a></li>
+                            @endif
                             <li class="list-group-item"><strong>POSTS</strong></li>
                             <li class="list-group-item"><a href="{{ route('posts') }}">All Posts</a></li>
                             <li class="list-group-item"><a href="{{ route('post.create') }}">New Post</a></li>
