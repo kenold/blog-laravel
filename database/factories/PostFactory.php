6825578e7e30707f582cc29da7fe900ddba4164b
Kenold Beauplan <bkenold@gmail.com>
2018-04-25 16:09:14 -0400

    generate fake slug from title

--- database/factories/PostFactory.php
+++ database/factories/PostFactory.php
@@ -3,9 +3,13 @@
 use Faker\Generator as Faker;
 
 $factory->define(App\Post::class, function (Faker $faker) {
+    
+    $title = $faker->sentence(6);
+    $slug = str_slug($title);
+
     return [
-        'title' => $faker->sentence(6),
-        'slug' => str_slug('title', '-'),
+        'title' => $title,
+        'slug' => $slug,
         'content' => $faker->paragraph(3),
         'category_id' => $faker->randomElement(['1', '2', '3', '4', '5']),
         'featured' => $faker->imageUrl(800, 400, 'people', true, 'Faker')
