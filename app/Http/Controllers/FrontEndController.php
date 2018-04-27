c9895f0ad1219e7fd218abeed20db868ef0cde11
Kenold Beauplan <bkenold@gmail.com>
2018-04-27 09:32:24 -0400

    initial commit

---
+++ app/Http/Controllers/FrontEndController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Setting, App\Category, App\Post, DB;
+
+class FrontEndController extends Controller
+{
+    public function index()
+    {        
+        return view('index')
+                    ->with('title', Setting::first()->site_name)
+                    ->with('categories', Category::take(5)->get())
+                    ->with('featured_post', Post::orderBy('created_at', 'desc')->first())
+                    ->with('second_post', Post::orderBy('created_at', 'desc')->skip(1)->take(1)->get()->first())
+                    ->with('third_post', Post::orderBy('created_at', 'desc')->skip(2)->take(1)->get()->first())
+                    ->with('news', Category::find(4))
+                    ->with('videos', Category::find(2));
+    }
+}
